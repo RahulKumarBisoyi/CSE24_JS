54ab632b7d6b28c50d6774a1e7754d27322fee8d
Rahul Kumar Bisoyi <rahulkumarbisoyi31@gmail.com>
2026-02-24 15:33:42 +0530

    type of data

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-console.log("hello world");
-document.write("hello world");
+console.log("hello world");  //this will show in console
+document.write("hello world");  //this will show on scrren
+        var num=10;
+        console.log(num);
+        console.log(typeof(num));
+        num=false;
+        console.log(typeof(num));
+        num="rahul";
+        console.log(typeof(num));
+        num=undefined;
+        console.log(typeof(num));
